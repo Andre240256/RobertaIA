4c29af6a2ec1f7a475143ecd45ed7480d99f6bd3
Andre240256 <andreassis240256@gmail.com>
2026-05-27 23:25:09 -0300

    chore: removed unnused duplicated training function

--- src/model_SAC_functions.py
+++ src/model_SAC_functions.py
@@ -60,44 +60,6 @@ def create_sac(env, lr, log_dir):
 
     return model
 
-def train_sac(lr, timesteps, seed):
-
-    set_seed(seed)
-
-    timestamp = time.strftime("%d/%m/%Y-%H %M %S")
-    log_dir = f"logs/SAC_Roberta_lr{lr}_seed{seed}_episodes{timesteps}_{timestamp}"
-    os.makedirs(log_dir, exist_ok=True)
-
-    env = make_vec_env("RobertaEnv-v0", n_envs=24, seed=seed)
-    eval_env = gym.make("RobertaEnv-v0")
-
-    eval_callback = EvalCallback(
-        eval_env, 
-        best_model_save_path=log_dir, 
-        log_path=log_dir,
-        eval_freq=100000,
-        deterministic=True, 
-        render=False
-    )
-
-    model = create_sac(env, lr, log_dir)
-
-
-    print(f"\n Training SAC on RobertaEnv-v0")
-    print(f"→ Learning Rate: {lr}")
-    print(f"→ Seed: {seed}")
-    print(f"→ Timesteps: {timesteps}")
-    print(f"→ Logs: {log_dir}\n")
-
-    model = create_sac(env, lr, log_dir)
-    model.learn(total_timesteps=timesteps, callback=eval_callback, progress_bar=True)
-
-    model_path = os.path.join(log_dir, f"SAC_Roberta_lr{lr}_seed{seed}.zip")
-    model.save(model_path)
-
-    print(f"\n Model saved to: {model_path}\n")
-    env.close()
-
 def retrain_sac(model_path: str, timesteps: int, seed: int):
     set_seed(seed)
     env = make_vec_env("RobertaEnv-v0", n_envs=16, seed=seed)
